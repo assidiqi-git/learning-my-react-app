090da25c419e2894b41f132525a3f6fc368b06e5
assidiqi-git <assidiqi1999@gmail.com>
2024-07-25 13:18:01 +0800

    card-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import Food from "./Food"
-import Footer from "./Footer"
-import Header from "./Header"
-
+import Card from "./Card";
 
 function App() {
   return (
     <>
-    <Header />
-      <Footer />
-      <Food/>
+      <Card />;
+      <Card />;
+      <Card />;
+      <Card />;
     </>
-)  
+  );
 }
 
-export default App
+export default App;
